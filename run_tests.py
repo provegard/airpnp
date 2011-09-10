256c8872d6a3231896f0c5c7570eb61c8a0ba1d4
Per Rovegård <per@rovegard.se>
2011-09-10 23:49:42 +0200

    Use nosetests for running tests; extracted separate CHANGELOG.

--- run_tests.py
+++
@@ -1,7 +0,0 @@
-# http://stackoverflow.com/questions/1896918/running-unittest-with-typical-test-directory-structure
-
-import unittest
-import test.all_tests
-testSuite = test.all_tests.create_test_suite()
-text_runner = unittest.TextTestRunner().run(testSuite)
-
